827f9b253fd9952352580ccacb291fe4a9c30103
Anderson Leoni <Anderson Leoni@Anderson>
2024-05-27 18:31:03 -0300

    nova aula

---
+++ src/variaveisJava.java
@@ -0,0 +1,20 @@
+
+	
+	public class variaveisJava {
+		public static void main(String[] args) {
+			
+			int maiorIdade = 18;
+			int idosoIdade = 60;
+			int jovemIdade = 16;
+			int criancaIdade = 7;
+			
+			int somaIdade;
+			
+			String cpf = "358.044.798-06";
+			
+			System.out.println("Maior idade é = " + maiorIdade);
+			System.out.println("Maior idade é = " + idosoIdade);
+			
+		}
+	}
+
