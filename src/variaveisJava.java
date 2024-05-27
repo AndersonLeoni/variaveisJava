14163a6550a58a8e230ea48531dfef6045ef73d5
Anderson Leoni <Anderson Leoni@Anderson>
2024-05-27 19:08:36 -0300

    variavel global

--- src/variaveisJava.java
+++ src/variaveisJava.java
@@ -1,20 +1,23 @@
 
 	
 	public class variaveisJava {
+		
+		/*variavel global é acessivel a todos */
+		static int maiorIdadeGlobal = 30;
+
 		public static void main(String[] args) {
 			
+			/*variavel local pertence somente a esse metodo */
 			int maiorIdade = 18;
-			int idosoIdade = 60;
-			int jovemIdade = 16;
-			int criancaIdade = 7;
+			System.out.println("Valor variavel local = " + maiorIdade);
 			
-			int somaIdade;
-			
-			String cpf = "358.044.798-06";
-			
-			System.out.println("Maior idade é = " + maiorIdade);
-			System.out.println("Maior idade é = " + idosoIdade);
+			metodo2();
 			
 		}
+		
+		public static void metodo2() {
+			System.out.println("Valor da variavel global = " + maiorIdadeGlobal);
+			}
+			
 	}
-
+	
