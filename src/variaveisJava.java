cc7e80eced56aeadbcb3ddff745f6cd0702b873c
Anderson Leoni <Anderson Leoni@Anderson>
2024-05-28 13:57:12 -0300

    app convertido para calculo de media

--- src/variaveisJava.java
+++ src/variaveisJava.java
@@ -1,23 +1,17 @@
 
-	
 	public class variaveisJava {
 		
-		/*variavel global é acessivel a todos */
-		static int maiorIdadeGlobal = 30;
 
 		public static void main(String[] args) {
 			
-			/*variavel local pertence somente a esse metodo */
-			int maiorIdade = 18;
-			System.out.println("Valor variavel local = " + maiorIdade);
+			int nota1 = 90;
+			int nota2 = 70;
+			int nota3 = 50;
+			int nota4 = 88;
+			int media = (nota1+nota2+nota3+nota4)/4;
 			
-			metodo2();
-			
+			System.out.println("Media das notas = " + media);
 		}
-		
-		public static void metodo2() {
-			System.out.println("Valor da variavel global = " + maiorIdadeGlobal);
-			}
 			
 	}
 	
