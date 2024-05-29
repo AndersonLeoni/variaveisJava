950c98ad7628903cc2b88a7452c92c7e3a6ef87d
Anderson Leoni <Anderson Leoni@Anderson>
2024-05-28 22:20:33 -0300

    Implementação do char

--- src/variaveisJava.java
+++ src/variaveisJava.java
@@ -8,9 +8,13 @@
 			int nota2 = 70;
 			int nota3 = 50;
 			int nota4 = 88;
+			
+			char sexoF = 'F';
+			char sexoM = 'M';
+			
 			int media = (nota1+nota2+nota3+nota4)/4;
 			
-			System.out.println("Media das notas = " + media);
+			System.out.println("Media das notas = " + media + " Aluno do sexo: " + sexoF);
 		}
 			
 	}
