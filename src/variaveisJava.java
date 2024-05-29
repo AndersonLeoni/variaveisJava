6be6e78b6f123c2fc10a6fe343ac1ac67b2903e7
Anderson Leoni <Anderson Leoni@Anderson>
2024-05-29 20:51:47 -0300

    Modificado para sistema de calculo de media

--- src/variaveisJava.java
+++ src/variaveisJava.java
@@ -1,20 +1,23 @@
 
-	public class variaveisJava {
-		
+public class variaveisJava {
 
-		public static void main(String[] args) {
-				
-			
-			int  mediaAluno = 69;
-			
-			if (mediaAluno >= 70) {
-				System.out.println("Aprovado");
-			} else
-			if (mediaAluno < 70) {
-				System.out.println("Reprovado");
-			}
 
+	public static void main(String[] args) {
+
+		int nota1 = 10;
+		int nota2 = 30;
+		int nota3 = 40;
+		int nota4 = 20;
+
+		int media = (nota1 + nota2 + nota3 + nota4)/4;
+
+		if (media >= 50) {
+			System.out.println("Aprovado");
+		} else if (media >= 30 && media <= 49) {
+			System.out.println("Recuperação");
+		} else {
+			System.out.println("Reprovado");
 		}
-			
+
 	}
-	
+}
